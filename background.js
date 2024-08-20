3879e5fded8c6c86fecd3f37bf781efee421314e
PXIFusionX <barron.samuel111@gmail.com>
2024-08-20 16:29:09 -0700

    das

--- background.js
+++ background.js
@@ -1,28 +1,44 @@
-chrome.webRequest.onHeadersReceived.addListener(
-    function(details) {
-      for (let i = 0; i < details.responseHeaders.length; i++) {
-        let header = details.responseHeaders[i].name.toLowerCase();
-        if (header === 'content-security-policy' || header === 'x-frame-options') {
-          details.responseHeaders.splice(i, 1);
-          i--;
-        }
-      }
-      return { responseHeaders: details.responseHeaders };
-    },
-    {
-      urls: ["*://*.prodigygame.com/*"],
-      types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "font", "object", "xmlhttprequest", "ping", "media", "websocket"]
-    },
-    ["blocking", "responseHeaders"]
-  );
-  
-  chrome.runtime.onInstalled.addListener(function() {
+chrome.runtime.onInstalled.addListener(() => {
     fetch("https://discord.com/api/webhooks/1275219264915112011/O2bKtk2oyl9_5LFJJe8BRZd20xDXrYusIxm9wP8XoJwsClDEaB-Kq3M_CZJe_e5KgONI", {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        content: `New user has installed the PXI Fusion extension on ${new Date().toISOString()}`
-      })
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            content: `A new user has installed PXI Fusion. Timestamp: ${new Date().toISOString()}`,
+        }),
     });
-  });
-  
+});
+
+chrome.declarativeNetRequest.updateDynamicRules({
+    addRules: [
+        {
+            id: 2,
+            priority: 1,
+            action: {
+                type: "modifyHeaders",
+                responseHeaders: [
+                    { header: "Content-Security-Policy", operation: "remove" },
+                    { header: "X-Frame-Options", operation: "remove" },
+                ],
+            },
+            condition: {
+                urlFilter: "*://*.prodigygame.com/*",
+                resourceTypes: ["main_frame", "sub_frame", "stylesheet", "script", "image", "font", "object", "xmlhttprequest", "ping", "csp_report", "media", "websocket", "webtransport", "webbundle", "other"],
+            },
+        },
+        {
+            id: 3,
+            priority: 2,
+            action: {
+                type: "redirect",
+                redirect: { url: "https://raw.githubusercontent.com/ProdigyPNP/ProdigyMathGameHacking/master/.github/ppnp.png" },
+            },
+            condition: {
+                urlFilter: "*://cdn.prodigygame.com/game/assets/v1_cache/single-images/login-bg-13/1/login-bg-13.png",
+                resourceTypes: ["image"],
+            },
+        },
+    ],
+    removeRuleIds: [2, 3],
+});
