7fa1bedaee4228f141c0fc7530cc285109374749
PXIFusionX <barron.samuel111@gmail.com>
2024-08-23 21:47:21 -0700

    123

---
+++ content.js
@@ -0,0 +1,5 @@
+// Store the PXI Version in localStorage
+localStorage.setItem('PXI Version', '1.0');
+
+// Optionally, you can log it to the console to verify
+console.log('PXI Version set to:', localStorage.getItem('PXI Version'));
